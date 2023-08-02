b13832a1cb8554b58f4cb092ebfcf7500fce2e15
hoang172006long <hoanglong172006@gmail.com>
2023-08-02 16:08:51 +0700

    second

--- main.js
+++ main.js
@@ -5,47 +5,158 @@ const btns = $$('.btn')
 const input = $('.screen')
 
 const calculator = {
+    squareRootOfTwo(newArrayValue) {  // hàm xử lý căn bậc 2
+        let squareRootOfTwoIndexArray = []
+        // lấy ra index của dấu căn và cho vào mảng
+        for (let i = 0; i < newArrayValue.length; i++) {
+            if (newArrayValue[i] === 'Math.sqrt(') {
+                squareRootOfTwoIndexArray.push(i)
+            } 
+        }
+        // tự động thêm dấu "*" khi trước dấu căn là 1 số
+        for (let i of squareRootOfTwoIndexArray) {
+            if (i != 0) {
+                newArrayValue.splice(i, 0, '*')
+            }
+        }
+    },
+    characterConversion(arrayValue,newArrayValue) {  
+        // hàm chuyển đổi các kí tự không hợp lệ thành hợp lệ và đẩy vào mảng newArrayValue
+        arrayValue.forEach(valueInArray => {
+            if (valueInArray === '^'){
+                valueInArray = '**'
+            }
+            if (valueInArray === '√('){
+                valueInArray = 'Math.sqrt('
+            } 
+            if (valueInArray === '÷') {
+                valueInArray = '/'
+            }
+            if (valueInArray === '×') {
+                valueInArray = '*'
+            }
+            newArrayValue.push(valueInArray)
+        })
+    },
+    handleNumber(allValueOfBtn,num) {
+        // nếu là số nguyên thì để nguyên
+        // nếu là số thập phân thì làm tròn tới chữ số thứ 5
+        let closingParenthesis = 0
+        let openingParenthesis = 0
+        let a ='';
+        for (let i of allValueOfBtn) {
+            if (i == '(') openingParenthesis++
+            if (i == ')') closingParenthesis++
+        }
+
+        if (closingParenthesis !== openingParenthesis) {
+            allValueOfBtn+=')'
+        } 
+        
+        if (allValueOfBtn.charAt(allValueOfBtn.length-1) === '+') {
+            allValueOfBtn.padEnd('0')
+        } else {
+            if (Number.isInteger(eval(allValueOfBtn))) {
+                input.value = eval(allValueOfBtn)
+            } else {
+                if (eval(allValueOfBtn)) {
+                    input.value = eval(allValueOfBtn).toFixed(num)
+                }
+            }
+        }
+    },
+    deleteFirstCharacter(arrayValue,newArrayValue) {
+        // nếu có dấu cách là phần tử đầu tiên thì xóa bỏ
+        if (arrayValue[0] === '' | newArrayValue[0] === '') {
+            arrayValue.shift()
+            newArrayValue.shift()
+        }
+    },
     handleEvent() {
-        let arrayValue = [];
-        let allValueOfBtn
+        /** chú ý
+         *  - arrayValue chứa icon user nhìn thấy
+         *  - newArrayValue chứa kí tự hợp lệ của hàm eval()
+         * */ 
+        const _this = this
+        let arrayValue = []; // mảng chứa tất cả các giá trị là icon của button như (1,2,3,^,√(,÷,×)
+        let newArrayValue = []; // mảng chứa tất cả các giá trị hợp lệ trong hàm eval() như (1,2,3,**,Math.srt(),/,*)
+        let allValueOfBtn // chuỗi chứa tất cả giá trị của mảng newArrayValue (dùng hàm join())
+        let index = 0 // index hiện tại của mỗi giá trị trong mảng
+
+        // lặp qua tất cả các nút
         btns.forEach(btn => {
             btn.onclick = function() {
+                function resetArray() {
+                    arrayValue = []
+                    newArrayValue = []
+                }
+                _this.deleteFirstCharacter(arrayValue,newArrayValue)
+
                 if (btn.value === '=') {
-                    allValueOfBtn = arrayValue.join('')
-                    if (Number.isInteger(eval(allValueOfBtn))) {
-                        input.value = eval(allValueOfBtn)
-                    } else {
-                        if (eval(allValueOfBtn)) {
-                            input.value = eval(allValueOfBtn).toFixed(5)
-                        }
-                    }
-                    arrayValue = []
+                    _this.characterConversion(arrayValue,newArrayValue)
+                    _this.squareRootOfTwo(newArrayValue)
+                    allValueOfBtn = newArrayValue.join('')
+                    _this.handleNumber(allValueOfBtn,5)
+                    
+                    // mỗi khi ấn dấu '=' thì thay thế kết quả cho tất cả các phần tử của mảng
+                    // console.log(newArrayValue)
+                    resetArray()
                     arrayValue.push(input.value)
-                    if (btn.value === 'C' | 'Del') {
-                        allValueOfBtn = ''
-                    }
-                } else if (btn.value === 'Del') {
-                    arrayValue = []
-                    input.value = arrayValue
-                } else if (btn.value === 'C') {
+                    newArrayValue.push(input.value)
+                } 
+                else if (btn.value === 'Del') {
+                    resetArray()
+                    input.value = []
+                } 
+                else if (btn.value === 'C') {
+                    index--
+                    if (index <= 0) index = 0
+
+                    // xóa phần tử cuối của 2 mảng
                     arrayValue.pop()
+                    newArrayValue.pop()
+
                     allValueOfBtn = arrayValue.join('')
+
+                    // đẩy arrayValue lên màn hình 
                     input.value = allValueOfBtn
+                    
                     if (!allValueOfBtn) {
-                        arrayValue = []
-                        input.value = arrayValue
+                        resetArray()
+                        input.value = []
                     }
-                } else {
-                    input.value += btn.value
-                    arrayValue.push(btn.value)
+                } 
+                else if ( 
+                // không cho phép ấn dấu tại vị trí đầu tiên, trừ dấu '√(' và '(' 
+                    (btn.value === '-' |
+                    btn.value === '+' | 
+                    btn.value === '×' | 
+                    btn.value === '/100' | 
+                    btn.value === '^' | 
+                    btn.value === ')' |
+                    btn.value === '÷' |
+                    btn.value === '/') && 
+                    (arrayValue[0] === '' || arrayValue[0] === undefined) |
+                    // không cho phép ấn các dấu liên tục
+                    ( arrayValue[index-1] === '+' | 
+                    arrayValue[index-1] === '-' | 
+                    arrayValue[index-1] === '×' | 
+                    arrayValue[index-1] === '÷' | 
+                    arrayValue[index-1] === '^' | 
+                    arrayValue[index-1] === '√(' |
+                    arrayValue[index-1] === '/100' ) ) 
+                    {
+                    // không làm gì cả
+                } 
+                else {
+                        index++
+                        input.value += btn.value
+                        arrayValue.push(btn.value)
                 }
             }
         });
-    },
-    start() {
-        this.handleEvent()
     }
 }
 
-calculator.start()
+calculator.handleEvent()
 
